c3739869705b2d9c44fa543504435b4405dea815
André Zanghelini <an_dz@simutrans-forum>
2016-07-05 10:18:27 -0300

    Make it work again
    Should have committed before

--- TrueFacebookLinks.user.js
+++ TrueFacebookLinks.user.js
@@ -14,35 +14,36 @@ var ttl = function TrueFacebookLink(){
 	document.addEventListener('DOMNodeInserted', makeTrueLinks, false);
 
 	function makeTrueLinks(event) {
-		var links = document.getElementsByTagName('a'),
-			realURL,
-			pattern;
+		var links = document.getElementsByTagName('a');
 		// change links
 		for (var i = links.length - 1; i >= 0; i--) {
+			// Facebook either has JS attributes attached to the element or event attached to it, we check it to perform the fastest solution
+			// If attributes just delete them
 			if (links[i].hasAttribute("onmousedown")) {
 				links[i].removeAttribute('onmousedown');
 
-				// Remove redirection and unescape
-				pattern = "http://www.facebook.com/l.php?u=";
-				realURL = unescape(links[i].href.replace(pattern,""));
-				
-				// change the url to the real url
-				links[i].href = realURL;
+				// Remove facebook redirection.
+				var facebookRedirectString = "http://www.facebook.com/l.php?u=";
+				var realHref = unescape(links[i].href.replace(facebookRedirectString,""));
+
+				// set the href tag to the original URL as it should be.
+				links[i].href = realHref;
 			}
 			if (links[i].hasAttribute("onclick")) {
-				// Extract real url from facebook's sneaky mouseover event handler
-				pattern = /LinkshimAsyncLink.swap\(this\, \"(.*)\"\);/;
-				realURL = unescape(pattern.exec(links[i].getAttribute("onmouseover"))[1]);
-				realURL = realURL.replace(/\\\//g,"/");
-				// For some reason, the % sign gets escaped into unicode like so: \u0025
-				// Manually unescaping the url below.
-				realURL = realURL.replace(/\\u0025([0-9a-fA-F]{2})/g,"%$1");
+				// Extract real url from facebook's sneaky mouseover event handler.
+				var refPattern = /LinkshimAsyncLink.swap\(this\, \"(.*)\".*/;
+				if (links[i].hasAttribute("onmouseover")) {
+					var realHref = unescape(refPattern.exec(links[i].getAttribute("onmouseover"))[1]);
+					realHref = realHref.replace(/\\\//g,"/");
+					// For some reason, the % sign gets escaped into unicode like so: \u0025
+					// Manually unescaping the url below.
+					realHref = realHref.replace(/\\u0025([0-9a-fA-F]{2})/g,"%$1");
+					// set the href tag to the original URL as it should be.
+					links[i].href = realHref;
 
-				// change the url to the real url
-				links[i].href = realURL;
-
-				links[i].removeAttribute("onclick");
-				links[i].removeAttribute("onmouseover");
+					links[i].removeAttribute("onclick");
+					links[i].removeAttribute("onmouseover");
+				}
 			}
 		}
 	}
