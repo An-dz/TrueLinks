442dc441067b858d6697cdf4c243dc9d288c1c18
André Zanghelini <an_dz@simutrans-forum>
2017-02-25 11:26:45 -0300

    https links not working
    Forgot to check for both http & https. Also an overall change for better
    readability and standard use.

--- TrueFacebookLinks.user.js
+++ TrueFacebookLinks.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @author			An_dz
-// @version			1.4
+// @version			1.5
 // @name			TrueFacebookLink
 // @description		Remove the Facebook tracking in links
-// @date			2016 September 15
+// @date			2017 February 25
 // @include			https://*.facebook.com/*
 // @include			http://*.facebook.com/*
 // @run-at			document-start
@@ -12,49 +12,55 @@
 // ==/UserScript==
 
 (function TrueFacebookLink(){
-	document.addEventListener('DOMNodeInserted', makeTrueLinks, false);
+	function changeLink(link) {
+		var realHref;
+		var facebookRedirectString;
+		// Facebook either has JS attributes attached to the element or event attached to it, we check it to perform the fastest solution
+		// If attributes just delete them
+		if (link.hasAttribute("onmousedown")) {
+			link.removeAttribute("onmousedown");
 
-	function makeTrueLinks(event) {
-		var links = document.getElementsByTagName('a');
-		// change links
-		for (var i = links.length - 1; i >= 0; i--) {
-			// Facebook either has JS attributes attached to the element or event attached to it, we check it to perform the fastest solution
-			// If attributes just delete them
-			if (links[i].hasAttribute("onmousedown")) {
-				links[i].removeAttribute('onmousedown');
+			// Remove facebook redirection.
+			facebookRedirectString = /https?:\/\/www\.facebook\.com\/l\.php\?u=/;
+			realHref = unescape(link.href.replace(facebookRedirectString, ""));
 
-				// Remove facebook redirection.
-				var facebookRedirectString = "http://www.facebook.com/l.php?u=";
-				var realHref = unescape(links[i].href.replace(facebookRedirectString,""));
-
+			// set the href tag to the original URL as it should be.
+			link.href = realHref;
+		}
+		else if (link.hasAttribute("onclick")) {
+			// Extract real url from facebook's sneaky mouseover event handler.
+			var refPattern = /LinkshimAsyncLink.swap\(this,\s"(.*)".*/;
+			if (link.hasAttribute("onmouseover")) {
+				realHref = unescape(refPattern.exec(link.getAttribute("onmouseover"))[1]);
+				realHref = realHref.replace(/\\\//g, "/");
+				// For some reason, the % sign gets escaped into unicode like so: \u0025
+				// Manually unescaping the url below.
+				realHref = realHref.replace(/\\u0025([0-9a-fA-F]{2})/g, "%$1");
 				// set the href tag to the original URL as it should be.
-				links[i].href = realHref;
-			}
-			else if (links[i].hasAttribute("onclick")) {
-				// Extract real url from facebook's sneaky mouseover event handler.
-				var refPattern = /LinkshimAsyncLink.swap\(this\, \"(.*)\".*/;
-				if (links[i].hasAttribute("onmouseover")) {
-					var realHref = unescape(refPattern.exec(links[i].getAttribute("onmouseover"))[1]);
-					realHref = realHref.replace(/\\\//g,"/");
-					// For some reason, the % sign gets escaped into unicode like so: \u0025
-					// Manually unescaping the url below.
-					realHref = realHref.replace(/\\u0025([0-9a-fA-F]{2})/g,"%$1");
-					// set the href tag to the original URL as it should be.
-					links[i].href = realHref;
+				link.href = realHref;
 
-					links[i].removeAttribute("onclick");
-					links[i].removeAttribute("onmouseover");
-				}
-			}
-			else if (links[i].href.search("http://l.facebook") == 0) {
-				// Remove facebook redirection.
-				var facebookRedirectString = "http://l.facebook.com/l.php?u=";
-				var realHref = links[i].href.replace(facebookRedirectString,"")
-				realHref = unescape(realHref.substring(0, realHref.search("&")));
-
-				// set the href tag to the original URL as it should be.
-				links[i].href = realHref;
+				link.removeAttribute("onclick");
+				link.removeAttribute("onmouseover");
 			}
 		}
+		else if (link.href.search(/:\/\/l\.facebook/) > 0) {
+			// Remove facebook redirection.
+			facebookRedirectString = /https?:\/\/l\.facebook\.com\/l\.php\?u=/;
+			realHref = link.href.replace(facebookRedirectString, "");
+			realHref = unescape(realHref.substring(0, realHref.search("&")));
+
+			// set the href tag to the original URL as it should be.
+			link.href = realHref;
+		}
 	}
+
+	function makeTrueLinks(event) {
+		var links = document.getElementsByTagName("a");
+		// Can't use forEach because Facebook for some reason breaks it
+		for (var i = links.length - 1; i >= 0; i--) {
+			changeLink(links[i]);
+		}
+	}
+
+	document.addEventListener("DOMNodeInserted", makeTrueLinks, false);
 })();
