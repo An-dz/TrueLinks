508a98c71e4bc7fac4f2e224e646f6c11f49b519
André Zanghelini <An-dz@users.noreply.github.com>
2018-11-09 13:45:22 -0200

    [Global] Fix not working

--- TrueLinks.user.js
+++ TrueLinks.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @author          An_dz
-// @version         1.1
+// @version         1.2
 // @name            TrueLink
 // @description     Removes basic tracking strategies on links
-// @date            2018 October 1
+// @date            2018 November 7
 // @include         *
 // @run-at          document-start
 // @grant           none
@@ -11,14 +11,16 @@
 // @downloadURL     https://github.com/An-dz/TrueLinks/raw/master/TrueLinks.user.js
 // ==/UserScript==
 
-(function TrueTweetLink(){
+(function TrueLink(){
 	function makeTrueLinks(element) {
 		// only http is checked, other protocols are allowed
-		var links = element.querySelectorAll("a[href^='http'][href*='%3A%2F%2F']");
+		const links = element.querySelectorAll("a[href^='http'][href*='%3A%2F%2F']");
 		// change links
 		links.forEach(function (link) {
-			var match = link.href.match(/https?%3A%2F%2F[^&/]*/);
-			if (!!match && !(link.href.indexOf("login") < link.href.indexOf("%3A%2F%2F"))) {
+			const match = link.href.match(/https?%3A%2F%2F[^&/]*/);
+			const login = link.href.indexOf("login");
+
+			if (match && (login < 0 || login > link.href.indexOf("%3A%2F%2F"))) {
 				console.log("TrueLink", match);
 				link.href = unescape(match[0]);
 			}
@@ -26,7 +28,7 @@
 	}
 
 	// mutation observer is asynchronous, the link will load unchanged for a fraction of miliseconds but this should make the page more responsive
-	var observer = new MutationObserver(function (changes) {
+	const observer = new MutationObserver(function (changes) {
 		changes.forEach(function (chg) {
 			chg.addedNodes.forEach(function (element) {
 				if (element.parentElement !== null) {
